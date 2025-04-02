561688bfd1617bdbd517a2118d64d2e9ed4d50f1
Dienes Stein <dienes.stein@zitrus.com.br>
2025-04-02 20:12:02 -0300

    Agora os testes devem passar 100%

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,6 @@
 require('dotenv').config()
 
 const cucumber = require('cypress-cucumber-preprocessor').default
-const allureWriter = require('@shelex/cypress-allure-plugin/writer')
 const { defineConfig } = require("cypress")
 
 module.exports = defineConfig({
@@ -10,11 +9,17 @@ module.exports = defineConfig({
     specPattern: ["cypress/e2e/api/**/*.cy.js", "cypress/e2e/front/**/*.cy.js", "cypress/e2e/cucumber/features/*.feature"],
     setupNodeEvents(on, config) {
       on('file:preprocessor', cucumber())
-      allureWriter(on, config)
       config.env.email = process.env.CYPRESS_EMAIL
       config.env.password = process.env.CYPRESS_PASSWORD
       return config
     }
+  },
+  reporter: "mochawesome",
+  reporterOptions: {
+    reportDir: "cypress/report",
+    overwrite: true,
+    html: true,
+    json: true
   },
   experimentalInteractiveRunEvents: true
 })
