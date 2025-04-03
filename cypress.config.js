3f861ad41d72a0ff90669055c159a5107c17652f
Dienes Stein <dienes.stein@zitrus.com.br>
2025-04-02 22:54:00 -0300

    Agora os testes devem passar 100%

--- cypress.config.js
+++ cypress.config.js
@@ -7,16 +7,19 @@ module.exports = defineConfig({
   e2e: {
     baseUrl: "https://www.google.com.br",
     specPattern: ["cypress/e2e/api/**/*.cy.js", "cypress/e2e/front/**/*.cy.js", "cypress/e2e/cucumber/features/*.feature"],
-    reporter: "cypress-allure-plugin",
-    reporterOptions: {
-      resultDir: "cypress/results/allure"
-    },
     setupNodeEvents(on, config) {
       on('file:preprocessor', cucumber())
       config.env.email = process.env.CYPRESS_EMAIL
       config.env.password = process.env.CYPRESS_PASSWORD
       return config
     }
+  },
+  reporter: "mochawesome",
+  reporterOptions: {
+    reportDir: "cypress/report",
+    overwrite: true,
+    html: true,
+    json: true
   },
   experimentalInteractiveRunEvents: true
 })
